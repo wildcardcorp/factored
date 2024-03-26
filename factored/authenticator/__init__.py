b3671ee32b35d66f9e021d8d15033f1fe2e7cf0c
Wiessfelt, Katie <katie.wiessflet@wildcardcorp.com>
2024-03-26 14:56:22 -0500

    implement fm modify_response()

--- factored/authenticator/__init__.py
+++ factored/authenticator/__init__.py
@@ -9,9 +9,11 @@ from pyramid.view import view_config
 
 from factored.plugins import get_manager, get_plugin
 
+from factored_manager.plugins.FactoredManagerEmailAuth import FactoredManagerEmailAuth
+
 import logging
 logger = logging.getLogger('factored.authenticator')
-
+auditlog = logging.getLogger('factored.audit')
 
 def generate_jwt(settings, subject):
     cname = settings.get("jwt.cookie.name", "factored")
@@ -42,7 +44,7 @@ def generate_jwt(settings, subject):
 def get_authtype(req):
     # auth_type is derived from either the auth options submit button, or
     # the normal form submission (hidden value)
-    submit = req.params.get("submit", None)
+    submit = req.params.get("submitbtn", None)
     if submit is not None:
         if submit.startswith("authtype_"):
             auth_type = submit[9:]
@@ -92,7 +94,6 @@ def authenticate(req):
     auth_type = get_authtype(req)
 
     authenticators = plugins.getPluginsOfCategory("authenticator")
-
     # authentication plugins activated:
     auth_options = [dict(value=a.name, display=a.plugin_object.display_name)
                     for a in authenticators]
@@ -115,13 +116,13 @@ def authenticate(req):
         "auth_options": auth_options,
         "src": req.params.get("src", ""),
     }
-    if auth_type is not None:
+    if auth_type is not None and auth_type.strip() != '':
         tmpl_kwargs["authtype"] = auth_type
 
     # if we have a valid auth type selected by the user, then get it's 
     # configured template info too
     tmpl = "base.html"
-    if auth_type is not None and auth_type == "regform":
+    if auth_type == "regform":
         if registrar is not None:
             registrar_tmpl_kwargs = registrar.plugin_object.handle(
                 host,
@@ -155,6 +156,7 @@ def authenticate(req):
                     resp = Response(
                         body="Successfully authenticated, redirecting now...",
                         status=302)
+                    auditlog.info('Successfully authenticated, redirecting now...')
                     src = urlparse(req.params.get("src", "/"))
                     goodsrc = (req.scheme, req.host, src.path, src.params, src.query, src.fragment)
                     resp.location = urlunparse(goodsrc)
@@ -165,7 +167,7 @@ def authenticate(req):
                         secure=csec,
                         httponly=chttponly,
                         overwrite=True)
-                    return resp
+                    return FactoredManagerEmailAuth.modify_response(resp=resp, host=host, settings=settings, plugins=plugins, email=subject)
                 tmpl_kwargs.update(auth_tmpl_kwargs)
             auth_tmpl_str = auth_plugin.plugin_object.template(host, auth_tmpl_settings, req.params)
             tmpl = "authtype.html"
@@ -176,60 +178,8 @@ def authenticate(req):
         loader=jinja2.DictLoader(loader),
         autoescape=jinja2.select_autoescape(['html', 'xml']))
     tmpl_rendered = tmpl_env.get_template(tmpl).render(**tmpl_kwargs)
-
-    return Response(body=tmpl_rendered, status=200)
-
-
-@view_config(route_name='status')
-def status(req):
-    # check that we can get the plugin manager
-    host = req.domain
-    reqsettings = req.registry.settings
-    plugin_manager = reqsettings.get("plugins.manager", None)
-    if plugin_manager is None:
-        logger.error("plugin manager not configured")
-        return Response(status=500)
-
-    # check that we can get all the appropriate settings
-    sp, _ = get_plugin("plugins.settings", "settings", reqsettings, plugin_manager)
-    settings = {}
-    settings.update(req.registry.settings)
-    if sp is None:
-        logger.error("no settings plugin found")
-        return Response(status=500)
-    else:
-        try:
-            settings.update(sp.plugin_object.get_request_settings(host))
-        except Exception:
-            logger.error("couldn't fetch settings through settings plugin", exc_info=True)
-            return Response(status=500)
-
-    # check we can get the datastore
-    ds, _ = get_plugin("plugins.datastore", "datastore", settings, plugin_manager)
-    if ds is None:
-        logger.error("no data store plugin found")
-        return Response(status=500)
-
-    # check we can get the finder
-    finder, _ = get_plugin("plugins.finder", "finder", settings, plugin_manager)
-    if finder is None:
-        logger.error("no finder plugin found")
-        return Response(status=500)
-    else:
-        try:
-            # don't care if it's valid or not, just that it executes without exception
-            finder.plugin_object.is_valid_subject(host, "__notarealsubject__")
-        except Exception:
-            logger.error("problem checking subject validity", exc_info=True)
-            return Response(status=500)
-
-    # check we can get the template
-    template, _ = get_plugin("plugins.template", "template", settings, plugin_manager)
-    if template is None:
-        logger.error("no template plugin found")
-        return Response(status_code=500)
-
-    return Response(status=200)
+    resp = Response(body=tmpl_rendered, status=200)
+    return resp
 
 
 def app(global_config, **settings):
@@ -247,7 +197,6 @@ def app(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_mailer')
     config.add_route('authenticate', '/')
-    config.add_route('status', '/authenticator-status')
     config.scan('factored.authenticator')
     app = config.make_wsgi_app()
     return app
